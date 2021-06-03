68dd2197a987c7d0b56c7e6b747fb064e689fd93
luisble <luisble@gmail.com>
2021-06-03 18:12:03 -0300

    Criada a tabela Evento e altrerado o Admin

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Projeto Agenda
 
-Projeto de Agenda
-Python com Django
+**Projeto de Agenda utilizando Python e Django**
+
+Base SQlITE3
+
+Admin
+usr luisble
+pwd Minh@Agenda123
+
+User Read Only
+usr UsuarioReadOnly
+pwd Minh@Agenda123
--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
+from core.models import Evento
 from django.contrib import admin
-
+from core.models import Evento
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao', 'data_alteracao')
+    list_filter = ('titulo','usuario',)
+
+admin.site.register(Evento, EventoAdmin)
